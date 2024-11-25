0289829876250ece97847ef1e5b5ddf652144e88
Ahmed <amartazanov892@gmail.com>
2024-11-25 22:25:35 +0300

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import {createHashRouter, RouterProvider} from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const router = createHashRouter([
+  {
+      path: '/*',
+      element: <App/>
+  }
+])
+  
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <RouterProvider router={router} />
+  )
