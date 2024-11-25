aeafb8df4c9fabb616415806c4c5a4ebfa7b2283
Ahmed <amartazanov892@gmail.com>
2024-11-25 22:39:07 +0300

    commit 5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import {createHashRouter, RouterProvider} from 'react-router-dom';
 
 import Products from "./components/products/Products";
 import Product from "./components/product/Product";
 import NotFound from './components/notFound/notFound';
-import App from './App';
 
 const router = createHashRouter([
   {
