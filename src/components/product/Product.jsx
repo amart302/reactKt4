cb1fad61350746d0dc82472dfd8fb58dc7e73a9e
Ahmed <amartazanov892@gmail.com>
2024-11-25 22:45:05 +0300

    commit 9

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -11,7 +11,7 @@ export default function Product(){
     const navigate = useNavigate();
 
     function redirection(){
-        navigate("/products")
+        navigate("/")
     }
 
     useEffect(() => {
